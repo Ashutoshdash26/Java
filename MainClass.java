05673e8c8bf9a2e9b0957c341ad95059cf1eca2a
Ashutoshdash26 <dash98829@gmail.com>
2025-02-13 05:48:49 +0530

    student

--- MainClass.java
+++ MainClass.java
@@ -1,12 +1,12 @@
 class Student {
-    private String name; // Private variable
+    private String name; 
 
-    // Getter method
+    
     public String getName() {
         return name;
     }
 
-    // Setter method
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -14,8 +14,8 @@ class Student {
 public class MainClass {
     public static void main(String[] args) {
         Student s = new Student();
-        s.setName("John"); // Setting value using setter
-        System.out.println("Student Name: " + s.getName()); // Accessing using getter
+        s.setName("John"); 
+        System.out.println("Student Name: " + s.getName()); 
     }
 }
 
