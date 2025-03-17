97b1df7795cfab08e12aa0eb1788880cc0c2e4a7
Ashutoshdash26 <dash98829@gmail.com>
2025-03-17 06:34:29 +0530

    constructor

--- constructor.java
+++ constructor.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
+class construct{
+   int a,b,c;
+
+   construct(){
+    this.a=10;
+    this.b=20;
+    this.c=30;
+   }
+   construct(int m){
+      a=m;
+   }
+}
 public class constructor {
-    
+    int a,b,c; 
+    Scanner S=new Scanner(System.in);
+    constructor(){
+        System.out.println("enter a number");
+        a=S.nextInt();
+        b=50;
+        c=60;
+    }
+    public static void main(String[] args){
+          Scanner ss =new Scanner(System.in);
+          construct obj =new construct();
+          constructor obj1=new constructor();
+          System.out.println(obj.a+obj.b+obj.c);  
+          System.out.println(obj1.a+" "+obj1.b+" "+obj1.c );   
+          System.out.println("enter a number");
+
+          int m=ss.nextInt();
+          construct obj2=new construct(m);
+          System.out.println(obj2.a+m);
+
+    }
 }
