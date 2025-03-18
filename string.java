dec9bef33700eb1b64d24611a41865191ff00b35
Ashutoshdash26 <dash98829@gmail.com>
2025-03-18 06:24:00 +0530

    Strings

--- string.java
+++ string.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class string {
     public static void main(String[] args){
         Scanner ss= new Scanner(System.in);
@@ -7,6 +8,14 @@ public class string {
         String str1;
         str1=ss.nextLine();
         System.out.println(str1+" "+str.charAt(3));
+        String str2=new String();
+        str2 = ss.nextLine();
+        System.out.println(str2);
+        char c[]={'a','b','c','d','e'};
+        String S=new String(c);
+        System.out.println(S);
+        String sam = new String(str);
+        System.out.println(sam);
     }
 }
 
