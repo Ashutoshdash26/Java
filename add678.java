fb22ea9608b42691f4363c965306a4191753a0b7
Ashutoshdash26 <dash98829@gmail.com>
2025-03-20 22:12:00 +0530

    add 87

--- add678.java
+++ add678.java
@@ -1,13 +1,17 @@
 public class add678 {
     public static void main(String[] args){
-       for(int i=1;i<100;i+=2){
+       for(int i=1;i<500;i+=2){
         int k=1;
         //System.out.println(i);
         k=i;
         int m=1;
         int jjh=1;
+        int lol=0;
         while(k>0){
              m=k%10;
+             
+             lol=lol+m;
+
             if(m==0){
                 jjh=0;
                 break;
@@ -15,7 +19,7 @@ public class add678 {
             k=k/10;
             
         }
-        if(jjh==1){
+        if(jjh==1 && lol%2!=0){
             System.out.println(i);
         }
         
