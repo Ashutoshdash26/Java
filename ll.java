1ffac32c130cbe70f95eac54d32b53558be3937f
Ashutoshdash26 <dash98829@gmail.com>
2025-03-20 22:23:22 +0530

    add 88

--- ll.java
+++ ll.java
@@ -1,11 +1,14 @@
 public class ll {
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i += 2) { // Iterate through odd numbers only
+        for (int i = 1; i < 5000; i += 2) { // Iterate through odd numbers only
             int k = i;
+            int sumOfDigits = 0; // To store the sum of all digits
             boolean allDigitsOdd = true; // Assume all digits are odd initially
 
             while (k > 0) {
                 int m = k % 10; // Get the last digit
+                sumOfDigits += m; // Add the digit to the sum
+
                 if (m % 2 == 0) { // If the digit is even
                     allDigitsOdd = false; // Set flag to false
                     break; // Exit the loop
@@ -13,8 +16,9 @@ public class ll {
                 k = k / 10; // Remove the last digit
             }
 
-            if (allDigitsOdd) { // If all digits are odd
-                System.out.println(i); // Print the number
+            // Check if all digits are odd and the sum of digits is also odd
+            if (allDigitsOdd && sumOfDigits % 2 != 0) {
+                System.out.println(i);
             }
         }
     }
