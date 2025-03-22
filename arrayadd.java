6dd7c4cc304b87d3f9780e838e96eb60ebdaf0cc
Ashutoshdash26 <dash98829@gmail.com>
2025-03-22 21:39:40 +0530

    arrayadd

--- arrayadd.java
+++ arrayadd.java
@@ -5,11 +5,15 @@ public class arrayadd {
         Scanner ss=new Scanner(System.in);
         int s=ss.nextInt();
         int arr[]=new int[s];
+        System.out.println("Enter the elements ")
         for(int i=0;i<s;i++){
             arr[i]=ss.nextInt();
         }
         for(int i=0;i<s;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
         }
+        System.out.println("Enter the index you want to delete ");
+        int km=ss.nextInt();
+
     }
 }
