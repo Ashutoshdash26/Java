0d11cc2ac504f320a044bf7f80ed4a2516e1e203
Ashutoshdash26 <dash98829@gmail.com>
2025-05-04 22:50:39 +0530

    access modifiers

--- boxclass.java
+++ boxclass.java
@@ -5,15 +5,41 @@ class boxclass {
         System.out.println(a.a);
         int l = a.fun(2);
         System.out.println(l);
+        dll d = new dll();
+
+        System.out.println(d.fun());
+        System.out.println(d.a);
+        System.out.println(d.the);
+        System.out.println(d.king);
+        System.out.println(a.fun());
     }
 }
 
 class doo {
-
+    public int the = 3;
+    private int mos = 4;
+    protected int king = 5;
     int a = 10;
+
+    int fun() {
+
+        return mos;
+    }
 
     int fun(int a) {
         int b = this.a;
-        return b;
+        return mos;
+    }
+}
+
+class dll extends doo {
+    // int fun() {
+    // return a;
+    // }
+    // int fun() {
+    // return the;
+    // }
+    int fun() {
+        return king;
     }
 }
