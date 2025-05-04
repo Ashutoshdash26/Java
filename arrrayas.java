93b2a5f78e38bd00385808dea75c89cb3909a225
Ashutoshdash26 <dash98829@gmail.com>
2025-05-04 20:18:46 +0530

    StringarrayJack

--- arrrayas.java
+++ arrrayas.java
@@ -6,16 +6,16 @@ public class arrrayas {
         System.out.println("size");
         int size = sc.nextInt();
 
-        int arr[][] = new int[size][];
+        String arr[][] = new String[size][];
         for (int i = 0; i < arr.length; i++) {
             System.out.print(i + ":");
             int a = sc.nextInt();
-            arr[i] = new int[a];
+            arr[i] = new String[a];
         }
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(i + "  " + j + "=");
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = sc.nextLine();
             }
         }
         for (int i = 0; i < arr.length; i++) {
