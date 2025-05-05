aeae1b70651a3b2f9ada253bd9ede4a2d7479153
Ashutoshdash26 <dash98829@gmail.com>
2025-05-05 06:19:22 +0530

    Packages

---
+++ mypack/str87.java
@@ -0,0 +1,32 @@
+package mypack;
+
+public class str87 {
+    public static void main(String[] args) {
+        String sum = "totslsum";
+        int i = 10;
+        call c = new call();
+        c.fun(sum, i);
+        System.out.println(c);
+        String st = new String("Hello");
+        System.out.println(st);
+        System.out.println(st.charAt(4));
+        System.out.println(st.length());
+    }
+
+    public int add(int i, int k) {
+        return i + k;
+    }
+}
+class call{
+String asu;
+    int k;
+
+    void fun(String a, int b) {
+        asu = a;
+        k = b;
+    }
+
+    public String toString() {
+        return "king are " + asu + " " + k;
+    }
+}
--- packagess.java
+++ packagess.java
@@ -1,12 +1,16 @@
-import Mypack.String1;
+import mypack.str87;
+
 //package mypack;
 public class packagess {
     public static void main(String[] args) {
         b ob = new b();
         ob.show();
-        //String1 obb = new String1();
-        //int gam = odd.add(10, 20);
-       // System.out.println(gam);
+        // String1 obb = new String1();
+        // int gam = odd.add(10, 20);
+        // System.out.println(gam);
+        str87 obb = new str87();
+        int lad=obb.add(10, 20);
+        System.out.println(lad);
     }
 }
 
