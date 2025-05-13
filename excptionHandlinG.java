a6850d93abbcc9c5897c4f4d026c667c8f3af989
Ashutoshdash26 <dash98829@gmail.com>
2025-05-13 21:01:19 +0530

    Excption Handling

---
+++ excptionHandlinG.java
@@ -0,0 +1,42 @@
+public class excptionHandlinG {
+    public static void main ( String [] args ) {
+         try {
+            int[] arr={1,2,3,4,5};
+            int q=1;
+           // System.out.println(arr[q]);
+        //checkAge (17) ;
+        int age=9;
+        if(q>=arr.length){
+            throw new ArrayIndexOutOfBoundsException("not posible to acess");
+        }
+        else{
+            System.out.println("inside index");
+        }
+        if ( age < 18) {
+            throw new Exception ( " Age must be 18 or older ." ) ;
+           
+           }
+            else
+            {
+            System . out . println ( " Age > 18 " ) ;
+           }
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+        catch ( ArithmeticException e ) {
+         System . out . println ( e . getMessage () ) ;
+         }
+         catch(Exception e){
+            System.out.println(e.getMessage());
+         }
+         }
+        //  static void checkAge ( int age ) {
+        //  if ( age < 18) {
+        //  throw new ArithmeticException ( " Age must be 18 or older ." ) ;
+        // }
+        //  else
+        //  {
+        //  System . out . println ( " Age > 18 " ) ;
+        // }
+        // }
+}
