50146c071a929de3e531c0149e8317a9fb26b8fc
Ashutoshdash26 <dash98829@gmail.com>
2025-05-19 09:07:08 +0530

    Exception Handling Throws

--- ReThrow.java
+++ ReThrow.java
@@ -1,3 +1,29 @@
 public class ReThrow {
-    
+    public void fun(int a,int b) throws Exception{
+        try{
+            if(a<19||b>75){
+                System.out.println("Not Elegible");
+                throw new Exception("it is not maching");
+            }else{
+                System.out.println("eligable");
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+           throw e;
+           
+        }
+    }
+    public static void main(String[] args) {
+        try{
+
+        
+        ReThrow obj=new ReThrow();
+        obj.fun(20,77);
+        obj.fun(21,44);
+        obj.fun(34,65);
+        obj.fun(9,10 );
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
