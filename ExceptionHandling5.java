386db39f70572f019714d733b276ed1a83edad38
Ashutoshdash26 <dash98829@gmail.com>
2025-05-19 06:39:50 +0530

    Exception Handling

--- ExceptionHandling5.java
+++ ExceptionHandling5.java
@@ -1,10 +1,21 @@
 public class ExceptionHandling5 {
     public static void main(String []args){
         try{
-            int a=10/0;
-            System.out.println("he he");
+            int a=10/1;
+            System.out.println("the ");
+
+            try {
+                int c[]={1};
+                int b=args.length;
+                System.out.println(b+ c[42]);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }catch(ArithmeticException e){
+
+            System.out.println("devide by zero");
             System.out.println(e.getMessage());
+
         }
         System.out.println("hi");
     }
