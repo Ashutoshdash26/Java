83b43dc1f94bae0fb317658e891298e0b6cda94b
Ashutoshdash26 <dash98829@gmail.com>
2025-05-23 08:50:59 +0530

    File Handling

--- FileHandling.java
+++ FileHandling.java
@@ -1,11 +1,30 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Scanner;
 public class FileHandling {
     public static void main(String[] args) {
         try {
-            
-        } catch (Exception e) {
+            File file=new File("FHandling.txt");
+            if (file.createNewFile()) {
+                System.out.println("File created : "+file.getName());
+            } else {
+                System.out.println("File arlady exists "+file.getName());
+            }
+
+
+
+            Scanner s=new Scanner(System.in);
+            FileWriter writer=new FileWriter("FHandling.yxt");
+            System.out.println("input in file : "+ file.getName());
+            String st=s.nextLine();
+            writer.write(st);
+            writer.close();
+            System.out.println("Successfully wrote to the file");
+        } catch (IOException e) {
             // TODO: handle exception
+            System.out.println("An error occered");
+            e.printStackTrace();
         }
     }
 }
