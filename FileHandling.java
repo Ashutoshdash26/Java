738bfdfb0664bbb3488449e7c4e6e25daec9fd2f
Ashutoshdash26 <dash98829@gmail.com>
2025-05-23 08:18:13 +0530

    File Handling

---
+++ FileHandling.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+public class FileHandling {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
