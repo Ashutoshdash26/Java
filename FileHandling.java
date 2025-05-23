4eb728d39a5ad202c487c00c93b1ad00b6c6196a
Ashutoshdash26 <dash98829@gmail.com>
2025-05-23 09:34:36 +0530

    File Handling

--- FileHandling.java
+++ FileHandling.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import java.util.Scanner;
 public class FileHandling {
     public static void main(String[] args) {
@@ -15,12 +19,26 @@ public class FileHandling {
 
 
             Scanner s=new Scanner(System.in);
-            FileWriter writer=new FileWriter("FHandling.txt");
+            FileWriter writer=new FileWriter(file,true);
             System.out.println("input in file : "+ file.getName());
             String st=s.nextLine();
-            writer.write(st);
+            writer.write(st + "\n");  // ✅ Correct newline
+
             writer.close();
             System.out.println("Successfully wrote to the file");
+
+
+
+            System.out.println("Reading from the file ");
+            BufferedReader read=new BufferedReader(new FileReader("FHandling.txt"));
+            String line=read.readLine();
+            while(line!=null){
+                System.out.println(line);
+                line=read.readLine();
+            }
+            read.close();
+
+
         } catch (IOException e) {
             // TODO: handle exception
             System.out.println("An error occered");
