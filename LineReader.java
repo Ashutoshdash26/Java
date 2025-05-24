906ded270940460807ce514aa471accfae5b72c2
Ashutoshdash26 <dash98829@gmail.com>
2025-05-24 08:45:29 +0530

    Line Reader

--- LineReader.java
+++ LineReader.java
@@ -3,9 +3,10 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 public class LineReader {
     public static void main(String[] args){
-         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-         String s;
+         
          try {
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            String s;
             System.out.println("press stop to stop the Execution");
             do {
                  s=(String)br.readLine();
