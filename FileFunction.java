63330bf7e373d8664281710c11b26bc04a2842d3
Ashutoshdash26 <dash98829@gmail.com>
2025-05-24 21:01:44 +0530

    Useful methods from File class

--- FileFunction.java
+++ FileFunction.java
@@ -21,5 +21,19 @@ public class FileFunction {
           }else{
             System.out.println("File name is not correct");
           }
+
+        System.out.println("\n");
+           System.out.println("File name: " + f.getName());
+        System.out.println("Path: " + f.getPath());
+        System.out.println("Absolute path: " + f.getAbsolutePath());
+        System.out.println("Parent: " + f.getParent());
+        System.out.println("Exists: " + f.exists());
+
+        if (f.exists()) {
+            System.out.println("Is writable: " + f.canWrite());
+            System.out.println("Is readable: " + f.canRead());
+            System.out.println("Is a directory: " + f.isDirectory());
+            System.out.println("File Size in bytes: " + f.length());
+        }
     }
 }
