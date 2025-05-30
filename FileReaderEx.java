077a88feca426ca26f4616398a827269eaa7d817
Ashutoshdash26 <dash98829@gmail.com>
2025-05-30 08:50:36 +0530

    File Reader Writer

--- FileReaderEx.java
+++ FileReaderEx.java
@@ -26,11 +26,52 @@ public class FileReaderEx {
         System.out.println("Write in the File : ");
         String sit=S.nextLine();
         char [] buf=sit.toCharArray();
+        for(int i=0;i<buf.length;i++){
+            System.out.print(buf[i]);
+        }
+
+
+
+        String sat="This is Time ...";
+        char [] arr=new char[sat.length()];
+        sat.getChars(0, sat.length(), arr, 0);
+
+
+
+
         try {
             FileWriter fr=new FileWriter("ex.txt");
             fr.write(sc+"\n");
             fr.write(sit+"\n");
+            fr.write(buf);
+            fr.write("\n");
             fr.close();
+
+
+            FileWriter f1=new FileWriter("Filewriter1.txt");
+            for(int i=0;i<arr.length;i+=2){
+                f1.write(arr[i]);
+            }
+            f1.close();
+
+            FileWriter f2=new FileWriter("Filewriter2.txt");
+            f2.write(arr);
+            f2.close();
+
+
+            FileWriter f3=new FileWriter("FileWriter3.txt");
+            f3.write(arr,arr.length/2,arr.length/4);
+            f3.close();
+
+
+            FileReader f4=new FileReader("FileWriter3.txt");
+            int vg;
+            System.out.println();
+            while((vg=f4.read())!=-1){
+                System.out.print((char)vg);
+            }
+            
+            f4.close();
 
         } catch (Exception e) {
             // TODO: handle exception
