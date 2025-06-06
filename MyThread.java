132a1ecaf45ccbb57f7ed775c16cda2041995266
Ashutoshdash26 <dash98829@gmail.com>
2025-06-06 21:01:04 +0530

    Thread

---
+++ MyThread.java
@@ -0,0 +1,34 @@
+public class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread running: " + i);
+        }
+    }
+    
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();  // thread object
+        t1.start();  // starts the thread, calls run() internally
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main thread: " + i);
+        }
+    }
+}
+class MyRunnable implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Runnable running: " + i);
+        }
+    }
+    
+    public static void main(String[] args) {
+        MyRunnable r1 = new MyRunnable();
+        Thread t1 = new Thread(r1);  // wrap it in Thread
+        t1.start();  // start the thread
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main thread: " + i);
+        }
+    }
+}
+
