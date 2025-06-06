36a5e09f2ebaebd8fe820ec0423d6b2f47e15bb7
Ashutoshdash26 <dash98829@gmail.com>
2025-06-06 21:05:07 +0530

    Multi Threading

--- MyThread.java
+++ MyThread.java
@@ -10,7 +10,16 @@ public class MyThread extends Thread {
         t1.start();  // starts the thread, calls run() internally
         
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Main thread: " + i);
+            System.out.println("Main thread 1 : " + i);
+        }
+
+
+        MyRunnable r1 = new MyRunnable();
+        Thread t2 = new Thread(r1);  // wrap it in Thread
+        t2.start();  // start the thread
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main thread 2 : " + i);
         }
     }
 }
@@ -21,14 +30,14 @@ class MyRunnable implements Runnable {
         }
     }
     
-    public static void main(String[] args) {
-        MyRunnable r1 = new MyRunnable();
-        Thread t1 = new Thread(r1);  // wrap it in Thread
-        t1.start();  // start the thread
+    // public static void main(String[] args) {
+    //     MyRunnable r1 = new MyRunnable();
+    //     Thread t1 = new Thread(r1);  // wrap it in Thread
+    //     t1.start();  // start the thread
         
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Main thread: " + i);
-        }
-    }
+    //     for (int i = 1; i <= 5; i++) {
+    //         System.out.println("Main thread: " + i);
+    //     }
+    // }
 }
 
