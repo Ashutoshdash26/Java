8cbe176a14070680e122621fe066436ec4ea14f8
Ashutoshdash26 <dash98829@gmail.com>
2025-07-24 19:57:28 +0530

    Array

---
+++ Narr.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Narr {
+    public static void main(String [] args){
+       Scanner s=new Scanner(System.in);
+       System.out.println("Enter the Size of Array ");
+       int a=s.nextInt();
+       int arr[]=new int[a];
+       for(int i=0;i<arr.length;i++){
+        System.out.println("Enter the " +i+"th ellement ");
+        arr[i]=s.nextInt();
+       }
+       for(int i=0;i<arr.length;i++){
+        System.err.print(arr[i]);
+       }
+    }
+}
