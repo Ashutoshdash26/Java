eb8ef73076efe36a61b51e6968fc4451544ff43f
Ashutoshdash26 <dash98829@gmail.com>
2025-10-22 18:35:36 +0530

    expriment

--- exp.java
+++ exp.java
@@ -1,5 +1,8 @@
+import 
 public class exp {
     public static void main(String [] args){
-        System.out.println("Ashutosh Dash");
+        int a=21;
+        boolean boo=false;
+        System.out.println("Ashutosh Dash  " + a +"  "+boo);
     }
 }
