0d693bd106f3c189e1852b7ce71ae3cd7b4271a4
Ashutoshdash26 <dash98829@gmail.com>
2025-10-23 08:12:36 +0530

    Experiment

--- exp.java
+++ exp.java
@@ -1,8 +1,19 @@
-import 
+import java.util.Scanner;
 public class exp {
     public static void main(String [] args){
         int a=21;
         boolean boo=false;
         System.out.println("Ashutosh Dash  " + a +"  "+boo);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<arr.length;i++){
+            System.out.printf("Enter the %d th element %n",i+1);
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
     }
 }
